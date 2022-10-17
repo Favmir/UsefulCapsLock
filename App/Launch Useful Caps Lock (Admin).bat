17b2007bc66d828a9d909ad3e9fc2280f9519d4e
Favmir <37527099+Favmir@users.noreply.github.com>
2022-10-17 16:34:12 +0900

    Removed run as admin files

--- App/Launch Useful Caps Lock (Admin).bat
+++
@@ -1,6 +0,0 @@
-cls
-@echo "Make sure you already have AutoHotKey program installed from https://www.autohotkey.com !"
-@echo "Running the UsefulCapsLock program..."
-@echo off
-@start "" "%~dp0\UsefulCapsLock(Administrator).ahk" /autostart
-REM AutoHotKey.exe 0 "%~dp0\UsefulCapsLock(Administrator).ahk" %~dp0
--- App/UsefulCapsLock(Administrator).ahk
+++
@@ -1,5 +0,0 @@
-﻿If !A_IsAdmin
-	Run *RunAs "%A_AhkPath%" "%A_ScriptFullPath%"
-Process, Priority, , H
-
-#include %A_ScriptDir%\UsefulCapsLock.ahk
