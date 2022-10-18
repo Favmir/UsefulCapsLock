a504b86cbc132726b5416ed89a610de2cd72dd42
Favmir <37527099+Favmir@users.noreply.github.com>
2022-10-18 13:10:09 +0900

    Update Remove from Startup.bat

--- App/Remove from Startup.bat
+++ App/Remove from Startup.bat
@@ -1,7 +1,6 @@
 cls
 @echo "Deleting shortcuts in %userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ ..."
 @echo off
-powershell Remove-Item '%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Useful Caps Lock (Admin).lnk'
 powershell Remove-Item '%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Useful Caps Lock.lnk'
 cls
 @echo Now Useful Caps Lock will no longer launch upon restart.
